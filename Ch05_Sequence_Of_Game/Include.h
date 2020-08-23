6a9e1a672312fae12b2903e9cd83793e792d0a6f
seungCheolCha <cscculture95@gmail.com>
2020-08-23 14:07:24 +0900

    챕터05(시퀀스 전이)를 위한 프로젝트 생성 및 이를 위한 라이브러리, 포함파일 동봉

---
+++ Ch05_Sequence_Of_Game/Include.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// 이 파일은 포함 파일들을 추가하기 위한 헤더 파일입니다.
+// C/C++ 런타임 헤더
+#include <fstream>
+#include <algorithm>
+
+// 프레임워크 관련 헤더
+#include "GameLib/Framework.h"
+#include "GameLib/GameLib.h"		// 매크로 관련 (SAFE_DELETE, ASSERT, STRONG_ASSERT, HALT)
+using namespace GameLib;
